911050e0faf5433c5d910b698393bb5f8e598f0e
Varunsn9 <104562883+Varunsn9@users.noreply.github.com>
2022-12-15 23:17:45 +0530

    new transformers file

--- src/test/java/Transformers_edge_test.java
+++ src/test/java/Transformers_edge_test.java
@@ -1,6 +1,8 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,20 +12,40 @@ import org.testng.annotations.Test;
 public class Transformers_edge_test extends Base_Class {
 	
 	@Test
-	public void test() {
-		 EdgeOptions op=new EdgeOptions();
-         op.setCapability("InPrivate", true);
-		driver = new EdgeDriver(op);
-		driver.get("https://www.youtube.com/watch?v=9elBYEpnPQw");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("//button[@aria-label='Play']")).click();
-		String title=driver.getTitle();
-		for(int i=0;i<2;i++)
-		if(!title.equalsIgnoreCase(driver.getTitle())) {
-			driver.quit();
-		}else {
-			i=0;
+	public void Transformers() throws InterruptedException {
+
+		
+		ChromeOptions opt=new ChromeOptions();
+		opt.addArguments("--incognito");
+		driver=new ChromeDriver(opt);	
+//		OperaOptions op=new OperaOptions();
+//		op.addArguments("--incognito");
+//		driver=new OperaDriver(op);
+		driver.manage().window().maximize();
+		try{
+			driver.get("https://www.youtube.com");
+			try {
+				driver.findElement(By.xpath("//button[@aria-label='Play']")).click();
+			}catch(Exception e) {
+				driver.navigate().refresh();
+				driver.findElement(By.xpath("//button[@aria-label='Play']")).click();
+			}
+		}catch(Exception e){
+			driver.navigate().refresh();
+			Thread.sleep(3000);
 		}
-	System.out.println("build completed");
+		//driver.findElement(By.xpath("//button[@class='ytp-large-play-button ytp-button']")).click();
+		String currentTitle=driver.getTitle();
+		for (int i=0;i<2;i++) {
+			if(!driver.getTitle().contentEquals(currentTitle)) {
+				driver.quit();
+			}else {
+				Thread.sleep(10000);
+				i=0;
+			}
+			
+		}	
+		System.out.println("built complete");
+		
 	}
 }
